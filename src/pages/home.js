11a2855e7d493446905e5b49b6e3b4873ff3a4bf
yuranan <yuranantiwa97@students.amikom.ac.id>
2024-05-13 17:13:14 +0700

    menambahkan di home

--- src/pages/home.js
+++ src/pages/home.js
@@ -9,6 +9,24 @@ const Home = () => {
       <div className="container-home">
         <div className="bg-1">
           <button className="btn-play"><b>Play Now</b></button>
+
+        </div>
+      <div className="profile">
+        <div className="bg-2">
+          <div className="character"></div>
+          <button className="btn-edit-character"><b>Edit Your Character</b></button>
+        </div>
+          <div className="stats"> 
+            <p><b>Your name character</b></p>           
+            <p><b>\nYour name</b></p>
+            <p><b>\nlevel\n</b></p> 
+            <div className="level"> </div> 
+            <p><b>\nYour skill</b></p> 
+            <div className="skill"></div>
+            <div className="experience">
+              <b>\nYour experience</b>
+            </div>
+          </div>
         </div>
       </div>
     </div>
