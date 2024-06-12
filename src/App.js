25921ce14861474b9cfa78a00df140ab84c98aa5
unknown <farhanhidayatul2002@gmail.com>
2024-06-12 22:46:43 +0700

    menambah

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Loading from './pages/loading'
-import Home from './pages/home'
-import Game from './pages/game'
-import Tutorial from './pages/tutorial'
-import About from './pages/about'
-import NT from './pages/notfound'
+import React, { useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import ReactGA from 'react-ga4';
+import Loading from './pages/loading';
+import Home from './pages/home';
+import Game from './pages/game';
+import Tutorial from './pages/tutorial';
+import About from './pages/about';
+import NT from './pages/notfound';
+
+// Initialize Google Analytics
+const TRACKING_ID = "G-FLNMFT2FES"; // Your Google Analytics tracking ID
+ReactGA.initialize(TRACKING_ID);
+
+const usePageViews = () => {
+  const location = useLocation();
+  useEffect(() => {
+    ReactGA.send({ hitType: "pageview", page: location.pathname + location.search });
+  }, [location]);
+}
 
 const App = () => {
+  usePageViews();
+
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Loading/>} />
-        <Route path="/home" element={<Home/>} />
-        <Route path="/game" element={<Game/>} />
-        <Route path="/tutorial" element={<Tutorial/>} />
-        <Route path="/about" element={<About/>} />
-        <Route path="*" element={<NT/>} />
+        <Route path="/" element={<Loading />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/game" element={<Game />} />
+        <Route path="/tutorial" element={<Tutorial />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<NT />} />
       </Routes>
     </Router>
   );
